b8b6939e5188a5b3d1779e73efa167dee4d87dd5
hosamyehya3 <hosamyehya3@gmail.com>
2026-07-06 20:43:30 +0200

    finish the design

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+const addinfo = document.getElementById("addinfo");
+const main = document.getElementById("main");
+const nav = document.getElementById("nav");
+const footer = document.getElementById("footer");
+const CardForm = document.querySelector('.CardForm');
+const xmark = document.querySelector(".xmark");
+const close = document.getElementById("close");
+addinfo?.addEventListener("click" , ()=>{
+main?.classList.add("hidden");
+nav?.classList.add("hidden");
+footer?.classList.add("hidden");
+document.body.classList.add("linear");
+CardForm?.classList.remove("d-none");
+
+})
+xmark?.addEventListener("click" , ()=>{
+   main?.classList.remove("hidden");
+nav?.classList.remove("hidden");
+footer?.classList.remove("hidden");
+document.body.classList.remove("linear");
+CardForm?.classList.add("d-none"); 
+})
+close?.addEventListener("click" , ()=>{
+      main?.classList.remove("hidden");
+nav?.classList.remove("hidden");
+footer?.classList.remove("hidden");
+document.body.classList.remove("linear");
+CardForm?.classList.add("d-none");  
+})
+document.body.addEventListener("click" , (e)=>{
+if ( e.target === e.currentTarget) {
+          main?.classList.remove("hidden");
+nav?.classList.remove("hidden");
+footer?.classList.remove("hidden");
+document.body.classList.remove("linear");
+CardForm?.classList.add("d-none");
+}
+})
